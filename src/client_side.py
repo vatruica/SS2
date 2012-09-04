c96b0ddbd28a39d2fb2a9ccff25f6bc64532333a
victor1tnet <romanescu3@gmail.com>
2012-09-04 09:53:00 +0200

    created the client socket and made it connect to the server

--- src/client_side.py
+++ src/client_side.py
@@ -3,3 +3,15 @@ Created on Sep 4, 2012
 
 @author: art3
 '''
+import socket
+
+#creating the socket
+clientsoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#connecting to our server
+clientsoc.connect(('127.0.0.1',5432))
+#sending a message
+clientsoc.send("heyy, this is client")
+#receiving the answer
+clientsoc.recv(100)
+#closing the socket
+clientsoc.close()
