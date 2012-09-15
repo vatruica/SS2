078c6baba56d63d61931e5487d95524f4733254d
victor1tnet <romanescu3@gmail.com>
2012-09-15 12:47:19 +0100

    integrated the client_side into the gui file

--- src/gui.py
+++ src/gui.py
@@ -1,26 +1,90 @@
 import Tkinter
 from Tkinter import *
-import client_side as cl
+import client_side as CS
+import socket
 
+import Tkinter as tk
+
+HOST='127.0.0.1'
+PORT=5432
+
+class guiclass():
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+        self.other = tk.Toplevel()
+        self.other.title("Victors Window")
+        self.otherlabel = tk.Label(self.other, text='SEND COMMAND TO RS485', relief = tk.RIDGE)
+        self.otherlabel.pack(side=tk.TOP, fill = tk.BOTH, expand = tk.YES)
+        self.otherbutton = tk.Button(self.other, text='TURN ON', command=self.send_on).pack(side=tk.TOP)
+        self.otherbutton = tk.Button(self.other, text='TURN OFF', command=self.send_off).pack(side=tk.TOP)
+        self.otherbutton = tk.Button(self.other, text='SEND', command=self.send_input).pack(side=tk.TOP)
+        self.otherbutton = tk.Button(self.other, text='QUIT', command=self.other.quit).pack(side=tk.BOTTOM)
+        self.outputvalue = tk.StringVar()
+        self.otherlabel1 = tk.Label(self.other, textvariable =self.outputvalue).pack(side=tk.BOTTOM)
+        
+        self.e = tk.Entry(self.other)
+        self.e.pack(side=tk.TOP) 
+
+    def send_on(self):
+        #creating the socket
+        cl_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cl_soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #connecting to our server
+        cl_soc.connect((HOST,PORT)) 
+        MSG = '#020002'
+        cl_soc.send(str(MSG))
+        #receiving the answer
+        #print( cl_soc.recv(100) )
+        self.outputvalue.set(str(cl_soc.recv(100)))
+        cl_soc.close()
+        
+    def send_off(self):
+        #creating the socket
+        cl_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        cl_soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #connecting to our server
+        cl_soc.connect((HOST,PORT)) 
+        MSG = '#020000'
+        cl_soc.send(str(MSG))
+        #receiving the answer
+        self.outputvalue.set(str(cl_soc.recv(100)))
+        cl_soc.close()
+    
+    def send_input(self):       
+        #creating the socket
+        cl_soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        
+        cl_soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        #connecting to our server
+        cl_soc.connect((HOST,PORT))   
+        #sending a message
+        #MSG = raw_input("write here:")
+        if self.e.get() != 'exit':
+            cl_soc.send(self.e.get())
+            #receiving the answer
+            #print( cl_soc.recv(100) )
+            self.outputvalue.set(str(cl_soc.recv(100)))
+            #closing the socket
+            cl_soc.close()
+        else:
+            cl_soc.close()
+            exit()
+
+        
 if __name__ == '__main__':
-    #defining the frame
-    win = Frame()
-    win.pack( )
     
-    #label
-    Label(win, text="Send your command to the RS485").pack(side=TOP)
+    win = tk.Frame()
+    win.pack()
     
-    #button widget
-    Button(win, text="Quit", command=win.quit).pack(side=BOTTOM)
     
-    #button widget
-    Button(win, text="TURN ON", command=cl.send_on()).pack(side=BOTTOM)
-    Button(win, text="TURN OFF", command=cl.send_off()).pack(side=BOTTOM)
+    guiclass()
     
-    #entry widget
-    e = Entry(win)
-    e.pack(side=TOP)
-    e.get() # get the inputed text
-    
-    #ending
-    win.mainloop( ) 
+    win.mainloop( )
